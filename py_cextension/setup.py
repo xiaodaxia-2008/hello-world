3c724eb773b65c18234e2dc9ea1ddcbfeac37529
Zen <36122917+xiaodaxia-2008@users.noreply.github.com>
2019-01-30 11:43:09 +0800

    Update setup.py

--- py_cextension/setup.py
+++ py_cextension/setup.py
@@ -2,3 +2,9 @@ from distutils.core import setup, Extension
 
 setup(name="myModule", version="1.1",
       ext_modules=[Extension("myModule", ['test.c', ])])
+
+
+# 使用下面的命令生成库
+# python setup.py build
+# 使用下面的命令安装库
+# python setup.py install
