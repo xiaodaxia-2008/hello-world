4d81e35311c4f30a080981ca3c2a407a2590f206
Zen <36122917+xiaodaxia-2008@users.noreply.github.com>
2019-01-30 12:17:59 +0800

    enrich the test_module comment

--- py_cextension/test_module.py
+++ py_cextension/test_module.py
@@ -1,5 +1,25 @@
 import myModule
+import timeit
+from numba import jit
+
+
+@jit(nopython=True)
+def fib(n):
+    if n < 2:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+
+
+print(fib(10)) # the first time calling , let the fib be complied by jit.
 
 myModule.hellworld()
-x = myModule.fib(10)
-print(x)
+x = myModule.fib(1)
+
+# 1.16s with c-extension
+print(timeit.timeit("myModule.fib(32)", "import myModule", number=100))  
+
+# 108.85s without jit, 1.78s with jit
+print(timeit.timeit("fib(32)", "from __main__ import fib", number=100))
+
+# conclusion: jit is 1.5x c-extentison version, but pure python version is nearly 100x slower.
